f24e3ce1b1c05dbc55de3855a03203a9df3f6871
jobert <EpicLegitPlayer@gmail.com>
2020-10-08 20:52:39 +0800

    Borrowed Equipment Log Done

--- EquipmentBorrowingSystem/Displays/Staff/BorrowedEquipmentLog/BorrowedEquipmentLogGuiDisplay.cs
+++ EquipmentBorrowingSystem/Displays/Staff/BorrowedEquipmentLog/BorrowedEquipmentLogGuiDisplay.cs
@@ -14,21 +14,30 @@ namespace EquipmentBorrowingSystem.Views.Staff.BorrowedEquipmentLog
         private TableLayoutPanel tableLayoutBELog;
         private Panel TitlePanel;
         private Panel LogPanel;
-        private ComboBox requestStatus;
         private Label MCLHeader;
         private ListView RequestLog;
         private Label BELsub;
-        private Button BackButton;
-        List<EquipmentRequest> EquipmentRequests;
+        private Button BackButton;  
 
         public void BindModelToView()
-        {
-            
-            foreach (EquipmentRequest request in Model)
-            {
-                RequestLog.Items.Add(new ListViewItem(new string[] { request.RequestStatusID.ToString(), request.Id.ToString()}));
-            }
+        {          
+            RequestLog.View = View.Details;
+            RequestLog.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            RequestLog.Columns.Add("Status", 100);
+            RequestLog.Columns.Add("Borrower", 150);
+            RequestLog.Columns.Add("Equipment", 150);
+            RequestLog.Columns.Add("Condition", 75);
+            RequestLog.Columns.Add("Reason", 200);
+            RequestLog.Columns.Add("Date Borrowed", 150);
+            RequestLog.Columns.Add("Date Returned", 150);            
+            RequestLog.Columns.Add("Expected Return Date", 150);            
 
+            foreach (EquipmentRequest request in Model.ToList())
+            {                
+                RequestLog.Items.Add(new ListViewItem(new string[] { request.RequestStatus.Name, request.Borrower.Email, request.Equipment.Name, request.Equipment.EquipmentCondition.Name, request.Reason, request.DateBorrowed.ToString(), request.DateReturned.ToString(), request.ExpectedReturnDate.ToString()}));
+                
+            }            
+            
         }
 
         partial void InitializeComponent()
@@ -40,7 +49,6 @@ namespace EquipmentBorrowingSystem.Views.Staff.BorrowedEquipmentLog
             this.LogPanel = new System.Windows.Forms.Panel();
             this.BackButton = new System.Windows.Forms.Button();
             this.RequestLog = new System.Windows.Forms.ListView();
-            this.requestStatus = new System.Windows.Forms.ComboBox();
             this.tableLayoutBELog.SuspendLayout();
             this.TitlePanel.SuspendLayout();
             this.LogPanel.SuspendLayout();
@@ -55,9 +63,9 @@ namespace EquipmentBorrowingSystem.Views.Staff.BorrowedEquipmentLog
             this.tableLayoutBELog.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutBELog.Name = "tableLayoutBELog";
             this.tableLayoutBELog.RowCount = 2;
-            this.tableLayoutBELog.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.47619F));
-            this.tableLayoutBELog.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.52381F));
-            this.tableLayoutBELog.Size = new System.Drawing.Size(733, 420);
+            this.tableLayoutBELog.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.08547F));
+            this.tableLayoutBELog.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.91453F));
+            this.tableLayoutBELog.Size = new System.Drawing.Size(871, 468);
             this.tableLayoutBELog.TabIndex = 0;
             // 
             // TitlePanel
@@ -66,14 +74,14 @@ namespace EquipmentBorrowingSystem.Views.Staff.BorrowedEquipmentLog
             this.TitlePanel.Controls.Add(this.MCLHeader);
             this.TitlePanel.Location = new System.Drawing.Point(3, 3);
             this.TitlePanel.Name = "TitlePanel";
-            this.TitlePanel.Size = new System.Drawing.Size(727, 101);
+            this.TitlePanel.Size = new System.Drawing.Size(865, 88);
             this.TitlePanel.TabIndex = 0;
             // 
             // BELsub
             // 
             this.BELsub.AutoSize = true;
             this.BELsub.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BELsub.Location = new System.Drawing.Point(205, 49);
+            this.BELsub.Location = new System.Drawing.Point(289, 49);
             this.BELsub.Name = "BELsub";
             this.BELsub.Size = new System.Drawing.Size(315, 31);
             this.BELsub.TabIndex = 2;
@@ -84,26 +92,26 @@ namespace EquipmentBorrowingSystem.Views.Staff.BorrowedEquipmentLog
             // 
             this.MCLHeader.AutoSize = true;
             this.MCLHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MCLHeader.Location = new System.Drawing.Point(10, 10);
+            this.MCLHeader.Location = new System.Drawing.Point(84, 10);
             this.MCLHeader.Name = "MCLHeader";
             this.MCLHeader.Size = new System.Drawing.Size(707, 39);
             this.MCLHeader.TabIndex = 0;
             this.MCLHeader.Text = "Malayan Colleges Laguna Borrowing System";
             this.MCLHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.MCLHeader.Click += new System.EventHandler(this.MCLHeader_Click);
             // 
             // LogPanel
             // 
             this.LogPanel.Controls.Add(this.BackButton);
             this.LogPanel.Controls.Add(this.RequestLog);
-            this.LogPanel.Controls.Add(this.requestStatus);
-            this.LogPanel.Location = new System.Drawing.Point(3, 110);
+            this.LogPanel.Location = new System.Drawing.Point(3, 97);
             this.LogPanel.Name = "LogPanel";
-            this.LogPanel.Size = new System.Drawing.Size(727, 307);
+            this.LogPanel.Size = new System.Drawing.Size(865, 368);
             this.LogPanel.TabIndex = 1;
             // 
             // BackButton
             // 
-            this.BackButton.Location = new System.Drawing.Point(642, 269);
+            this.BackButton.Location = new System.Drawing.Point(774, 30);
             this.BackButton.Name = "BackButton";
             this.BackButton.Size = new System.Drawing.Size(75, 23);
             this.BackButton.TabIndex = 2;
@@ -115,27 +123,19 @@ namespace EquipmentBorrowingSystem.Views.Staff.BorrowedEquipmentLog
             // 
             this.RequestLog.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.RequestLog.HideSelection = false;
-            this.RequestLog.Location = new System.Drawing.Point(175, 30);
+            this.RequestLog.Location = new System.Drawing.Point(17, 3);
             this.RequestLog.MultiSelect = false;
             this.RequestLog.Name = "RequestLog";
-            this.RequestLog.Size = new System.Drawing.Size(436, 262);
+            this.RequestLog.Size = new System.Drawing.Size(751, 362);
             this.RequestLog.TabIndex = 1;
             this.RequestLog.UseCompatibleStateImageBehavior = false;
             this.RequestLog.View = System.Windows.Forms.View.Details;
-            // 
-            // requestStatus
-            // 
-            this.requestStatus.FormattingEnabled = true;
-            this.requestStatus.Location = new System.Drawing.Point(17, 30);
-            this.requestStatus.Name = "requestStatus";
-            this.requestStatus.Size = new System.Drawing.Size(121, 21);
-            this.requestStatus.TabIndex = 0;
             // 
             // BorrowedEquipmentLogGuiDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(757, 444);
+            this.ClientSize = new System.Drawing.Size(902, 492);
             this.Controls.Add(this.tableLayoutBELog);
             this.Name = "BorrowedEquipmentLogGuiDisplay";
             this.Text = "Form1";
@@ -149,10 +149,13 @@ namespace EquipmentBorrowingSystem.Views.Staff.BorrowedEquipmentLog
 
         private void BackButton_Click(object sender, EventArgs e)
         {
-
+            Environment.Exit(0);
         }
 
+        private void MCLHeader_Click(object sender, EventArgs e)
+        {
 
+        }
     }
 
     //   Replace with class name              Replace with model class
